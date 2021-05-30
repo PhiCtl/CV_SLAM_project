82045fff17769348361028f6ca7d65eada5c1389
PhiCtl <philippine.descourtils@epfl.ch>
2021-05-30 17:12:16 +0200

    added object tracker

--- ros_ws/src/cv_ros/src/main_ros.py
+++ ros_ws/src/cv_ros/src/main_ros.py
@@ -4,6 +4,7 @@ import __init__
 from cv_lib.src.object_detection import ObjectDetector
 from cv_lib.src.camera_listener import CameraListener
 from cv_lib.src.object_prediction import ObjectPredictor
+from cv_lib.src.tracker import Tracker
 from cv_lib.src.myUtils import draw_bboxes
 from cv_ros.msg import ObjectPose
 from geometry_msgs.msg import PoseStamped
@@ -34,6 +35,7 @@ class PosePublisher():
         self.msg.header.frame_id, self.msg.header.stamp, self.msg.header.seq, self.msg.object_type = "camera", rospy.Time.now(), seq_id, String(object_type)
 
         for centroid, plane, i in zip(centroid_coo, plane_vector_coo, range(len(centroid_coo))):
+            print('centroid: ', centroid, plane)
             p = PoseStamped()
             p.header.frame_id, p.header.seq = 'camera', i
             p.pose.position.x = centroid[0]
@@ -86,10 +88,11 @@ class PosePublisher():
 def run():
     publisher = PosePublisher()
 
-    # start camera listener and detector
+    # # start camera listener and detector
     camera = CameraListener()
     detector = ObjectDetector()
     predictor = ObjectPredictor(model_name='YOLOv5x')
+    tracker = Tracker()
     publisher.set_status()
 
     # counters
@@ -97,13 +100,19 @@ def run():
     n = 1
 
     # utils
-    flower_pos = []
-    plant_hold_pos = []
+    rs_status = String('ELSE') # to refine
 
     while not rospy.is_shutdown():
 
         # Wait for Robotic station status
-        rs_status = rospy.wait_for_message(topic_rs_status, String)  # will be always active, otherwise code stops here
+        try:
+            rs_status = rospy.wait_for_message(topic_rs_status, String, timeout=1)
+        except(rospy.exceptions.ROSException):
+            # If we were already in the STANDSTILL setting, we stay in standstill
+            # Othwerwise, we do nothing
+            if rs_status.data == 'STANDSTILL':
+                pass
+
         if rs_status.data == 'STANDSTILL':
 
             # Take picture
@@ -120,50 +129,44 @@ def run():
             detector.get_plane_orientation(camera, plot=False)
 
             # detect flowers
-            poses, found_flowers = predictor(camera.bgr_image, camera, conf = 0.3, verbose=True)
+            # poses, found_flowers = predictor(camera.bgr_image, camera, conf = 0.3, verbose=True)
 
             #If we found any plant holder
             if found_plantHolders:
 
-                plant_hold_pos.append([detector.coo, detector.planes])# I want dim = (i, N, 2, 3)
-
-
+                tracker.update(detector.coo, detector.planes, 'plant_holder')
                 # Make mean over n values
                 if i%n == 0:
-                    plant_hold_pos = np.array(plant_hold_pos)
-                    mean = plant_hold_pos.mean(axis=0)
-                    plant_hold_pos = []
-                    publisher.publish_pose(mean[0, :, :], mean[1, :, :], i, 'plant_holders')
+                    obj_list, type = tracker.get_object_list(type='plant_holder')
+                    publisher.publish_pose(obj_list[0], obj_list[1], i, 'plant_holders')
                 # This is a success
                 publisher.set_status('SUCCESS')
 
                 # Return number and type of detected objects
-                msg = str(len(detector.coo)) + ' plant holder(s) detected'
+                msg = str(tracker.nb_detected['plant_holder']) + ' plant holder(s) detected'
                 publisher.publish_obj_detected(msg)
 
                 # Reset detector and update counter
                 detector.reset()
                 i += 1
 
-            if found_flowers:
+            # if found_flowers:
+            #
+            #     tracker.update(poses['centroids_coo'][:,:3], poses['orientations'], 'flower')
+            #
+            #     if j%n == 0:
+            #         obj_list = tracker.get_object_list('flower')
+            #         publisher.publish_pose(obj_list[0], obj_list[1], j, 'flower')
+            #     # Update counter
+            #     j += 1
+            #     # publisher.publish_pose(poses['centroids_coo'][:,:3], poses['orientations'], j, 'flowers')
+            #
+            #     # Publish state and number of flower detected
+            #     publisher.set_status('SUCCESS')
+            #     msg = str(poses['nb_detected']) + ' flower(s) detected'
+            #     publisher.publish_obj_detected(msg)
 
-                flower_pos.append([poses['centroids_coo'][:,:3], poses['orientations']])
-
-                if j%n == 0:
-                    flower_pos = np.array(flower_pos)
-                    mean = flower_pos.mean(axis=0)
-                    publisher.publish_pose(mean[0,:,:], mean[1,:,:], j, 'flowers')
-                    flower_pos = []
-                # Update counter
-                j += 1
-                # publisher.publish_pose(poses['centroids_coo'][:,:3], poses['orientations'], j, 'flowers')
-
-                # Publish state and number of flower detected
-                publisher.set_status('SUCCESS')
-                msg = str(poses['nb_detected']) + ' flower(s) detected'
-                publisher.publish_obj_detected(msg)
-
-            if not found_flowers: #and not found_plantHolders:
+            if not found_plantHolders: # found_flowers and not found_plantHolders:
                 publisher.set_status('RETRY')
                 publisher.publish_obj_detected('None')
         publisher.publish_status()
